96a9715ddcd9f555e106a5a5b0384944c64c9b3a
TensorNd <do-not-mail@nothing.com>
2022-05-22 19:30:17 +0530

    L1 penalty based feature selection strategy.

---
+++ feature_selection.py
@@ -0,0 +1,65 @@
+from pandas import read_csv,read_excel,DataFrame
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import average_precision_score,make_scorer
+from numpy import linspace,abs,mean,where,array
+from warnings import filterwarnings
+
+filterwarnings("ignore")
+
+def read_data(path):
+    if path.endswith('.csv'):
+        return read_csv(path)
+    elif path.endswith('.xslx'):
+        return read_csv(path)
+    else:
+        return Exception()
+
+def prepareLists(data,exclusion_set=['Customer_ID','Product_Holding_B1','Product_Holding_B2','Customer_Category']):
+    target_cols=[col for col in data.columns if 'Product_Holding_B2_' in col]
+    features_under_consideration=list(set(data.columns) - set(exclusion_set) - set(target_cols))
+    selected_features={i : [] for i in target_cols}
+    return features_under_consideration, target_cols, selected_features
+
+def getThreshold(feature_importances):
+    # Create a non Naive threshold strategy.
+    return 0
+
+def lassoBasedSelection(data_path='./Dataset/preprocessed_train.csv'):
+    try:
+        data=read_data(data_path)
+    except:
+        print("Only csv and excel data is supported.")
+        exit(-1)
+    features_under_consideration, target_cols, selected_features = prepareLists(data)
+    scaler=StandardScaler()
+    scorer=make_scorer(average_precision_score)
+    X=scaler.fit_transform(data[features_under_consideration])
+    for i in range(len(target_cols)):
+        Y=data[target_cols[i]]
+        #HPT=GridSearchCV(Lasso(),{"alpha" : linspace(0.1,0.5,num=1000)},n_jobs=-1,scoring=scorer,cv=5,verbose=1)
+        HPT=Lasso(alpha=0.1)
+        HPT.fit(X,Y)
+        #print(f"Best parameter found for {target_cols[i]} is {HPT.best_params_}")
+        #feature_importances=abs(array(HPT.best_estimator_.coef_))
+        feature_importances=abs(array(HPT.coef_))
+        threshold=getThreshold(feature_importances)
+        #print(f"{target_cols[i]} Feature Importance stats => MIN : {min(feature_importances)}, MAX : {max(feature_importances)}, MEAN : {mean(feature_importances)}")
+        if max(feature_importances) > threshold:
+            features=array(features_under_consideration)[feature_importances > threshold]
+        else:
+            features=array(features_under_consideration)
+        selected_features[target_cols[i]].extend(list(features))
+    return selected_features
+
+def test():
+    selected=lassoBasedSelection()
+    max_length=max([len(selected[i]) for i in selected.keys()])
+    for i in selected.keys():
+        if len(selected[i]) < max_length:
+            selected[i].extend(list(['' for j in range(max_length - len(selected[i]))]))
+    DataFrame(selected).to_csv('./Dataset/selected_features.csv',index=False)
+
+if __name__=='__main__':
+    test()
